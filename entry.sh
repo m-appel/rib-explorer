81ba30ed47fa51e076401e9a0a74324082d6a7b7
Malte Tashiro <malte@iij.ad.jp>
2025-01-28 03:24:18 +0000

    Add command to execute full pipeline at once

--- entry.sh
+++ entry.sh
@@ -5,9 +5,15 @@ Help()
     echo "Create prefix-to-ASN mapping from RIS and Routeviews RIBs."
     echo
     echo "Full pipeline is index -> fetch -> transform -> create."
-    echo "All commands require at least a timestamp in the format YYYY-MM-DDTHH:MM."
-    echo "Options after the command are passed to the respective Python script."
-    echo "Use 'command -h' to see the available options for the command."
+    echo "All commands (except 'index') require at least a timestamp in the format YYYY-MM-DDTHH:MM."
+    echo "Options after the command are passed to the respective Python script (except for 'all', see below)."
+    echo "Use 'command -h' to see the available options for the other commands."
+    echo
+    echo "The 'all' command runs the entire pipeline and thus needs options for multiple scripts. By default"
+    echo "it only includes the option for --min-collector-count so if you want to use different options, run"
+    echo "the 'create' command by itself."
+    echo
+    echo "Syntax for 'all': all timestamp num-fetchers num-transformers min-collector-count output-file"
     echo
     echo "Syntax: command timestamp [options]"
     echo
@@ -26,16 +32,26 @@ Help()
 T="$1"
 case $T in
     index)
-        python3 build-index.py ${@:2}
+        python3 build-index.py
     ;;
     fetch)
-        python3 fetch-snapshots.py ${@:2}
+        python3 fetch-snapshots.py "${@:2}"
     ;;
     transform)
-        python3 transform-snapshots.py ${@:2}
+        python3 transform-snapshots.py "${@:2}"
     ;;
     create)
-        python3 create-merged-rtree.py ${@:2}
+        python3 create-merged-rtree.py "${@:2}"
+    ;;
+    all)
+        if [ $# -ne 6 ]; then
+            echo "usage: all timestamp num-fetchers num-transformers min-collector-count output-file"
+            exit 1
+        fi
+        python3 build-index.py
+        python3 fetch-snapshots.py -n "$3" "$2"
+        python3 transform-snapshots.py -n "$4" "$2"
+        python3 create-merged-rtree.py --min-collector-count "$5" "$2" "$6"
     ;;
     clean-data)
         rm -r data/*
